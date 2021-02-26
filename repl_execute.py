eac54e1affedb70429226bece67f67e19ecb36f7
Laniskefu <hansanyue@hotmail.com>
2021-02-26 14:07:33 +0800

    add argument deciding whether or not to output the token list

--- repl_execute.py
+++ repl_execute.py
@@ -3,7 +3,7 @@ from II_syntactic.syntactic_analyzer import Syntactic
 from interpreter import Interpreter
 
 
-def repl_execute(abstract_syntax_tree=False, variables=False):
+def repl_execute(token=False, node=False, var=False):
     """
     command line REPL（Read-Eval-Print Loop）
     support arithmetic, variable declaration and variable assignment
@@ -11,20 +11,22 @@ def repl_execute(abstract_syntax_tree=False, variables=False):
     interpreter = Interpreter()
     while True:
         statement = input(">> ")
-        # hard coding for quit
         if statement == "quit()" or statement == "exit()":
             break
         token_list = Lexical.statement_analyzer(statement)
+        if token:
+            for t in token_list:
+                print(t)
         ast_root = Syntactic.statement_analyzer(token_list)
-        if abstract_syntax_tree:
+        if node:
             if ast_root:
                 ast_root.dump()
             else:
                 print("ast_root is None")
         interpreter.interpret_statement(ast_root)
-        if variables:
+        if var:
             print(interpreter.get_variables())
 
 
 if __name__ == "__main__":
-    repl_execute(abstract_syntax_tree=False, variables=False)
+    repl_execute()
