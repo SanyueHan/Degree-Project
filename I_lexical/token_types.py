1980b5a79f151c2be50d63c8a833867bb9b13c79
Laniskefu <hansanyue@hotmail.com>
2021-03-05 01:48:10 +0800

    expand token_types

--- I_lexical/token_types.py
+++ I_lexical/token_types.py
@@ -6,23 +6,33 @@ class TokenType(Enum):
     # Keywords: if、else、int
     INT = re.compile("int")
 
+    # Identifier: age
+    ID = re.compile("[a-zA-Z_]([a-zA-Z_]|[0-9])*")
+
     # Literal: 123, "apple"
     NUM_LIT = re.compile(r"[0-9]+\.[0-9]+|[0-9]+")
     STR_LIT = re.compile(r"\".*\"|\'.*\'")
 
-    # Operators: +、-、=
+    # Logical Operators
+    AND = re.compile(r"&&")
+    OR = re.compile(r"\|\|")
+
+    # Relational Operators
+    EQ = re.compile("==")
+    NE = re.compile("!=")
+    GE = re.compile(">=")
+    GT = re.compile(">")
+    LE = re.compile("<=")
+    LT = re.compile("<")
+
+    # Arithmetic Operators
     PLUS = re.compile("[+]")
     MINUS = re.compile("-")
     STAR = re.compile("[*]")
     SLASH = re.compile("/")
 
-    GE = re.compile(">=")
-    GT = re.compile(">")
-    EQ = re.compile("==")
-    NE = re.compile("!=")
-    LE = re.compile("<=")
-    LT = re.compile("<")
-
+    # Other
+    ASSIGNMENT = re.compile("=")
     SEMICOLON = re.compile(";")
     L_PAREN = re.compile(r"\(")
     R_PAREN = re.compile(r"\)")
@@ -30,11 +40,6 @@ class TokenType(Enum):
     R_BRACKET = re.compile(r"]")
     L_BRACE = re.compile("{")
     R_BRACE = re.compile("}")
-
-    ASSIGNMENT = re.compile("=")
-
-    # Identifier: age
-    ID = re.compile("[a-zA-Z_]([a-zA-Z_]|[0-9])*")
 
     WHITESPACE = re.compile(r"\s+")
     ANNOTATION = re.compile("%.*")  # in '.', '\r' or '\n' is automatically excluded
@@ -46,6 +51,10 @@ if __name__ == "__main__":
         print(number_literal)
     if string_literal := re.findall(TokenType.STR_LIT.value, "\'apple\', \"banana\""):
         print(string_literal)
+    if and_operator := re.findall(TokenType.AND.value, "&&"):
+        print(and_operator)
+    if or_operator := re.findall(TokenType.OR.value, "||"):
+        print(or_operator)
     if whitespace := re.findall(TokenType.WHITESPACE.value, "1 2\f3\n4\r5\t6\v"):
         print(whitespace)
     if annotation := re.findall(TokenType.ANNOTATION.value, "int a = 10; % this is annotation"):
