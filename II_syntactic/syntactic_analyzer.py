94c904e4ebf5aedf9d73c97bf769267aec4dde7b
Laniskefu <hansanyue@hotmail.com>
2021-03-07 14:49:28 +0800

    optimise logic

--- II_syntactic/syntactic_analyzer.py
+++ II_syntactic/syntactic_analyzer.py
@@ -8,7 +8,6 @@ class Syntactic:
     def statement_analyzer(token_list):
         if ast_node := Parser(token_list[:]).parse_declaration_statement():
             return ast_node
-        # parsing assignment before parsing expression because of corner case like "a = 1"
         if ast_node := Parser(token_list[:]).parse_assignment_statement():
             return ast_node
         if ast_node := Parser(token_list[:]).parse_expression_statement():
@@ -17,8 +16,7 @@ class Syntactic:
 
     @staticmethod
     def program_analyzer(token_lists):
-        children = [Syntactic.statement_analyzer(token_list) for token_list in token_lists]
-        children = [child for child in children if child]
+        children = [Syntactic.statement_analyzer(token_list) for token_list in token_lists if token_list]
         ast_root = ASTNode(n_type=ASTNodeType.PROGRAM, children=children)
         return ast_root
 
