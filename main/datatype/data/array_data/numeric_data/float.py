d2eb3c8d9902420d72cd4da7d562b416590a878b
Laniskefu <hansanyue@hotmail.com>
2021-03-24 16:58:05 +0800

    optimise float data formatting logic

--- main/datatype/data/array_data/numeric_data/float.py
+++ main/datatype/data/array_data/numeric_data/float.py
@@ -6,31 +6,15 @@ class Float(Numeric):
     Class = DataClass.FLOAT
 
     def __str__(self):
-        if all(self.__is_whole(i) for i in self):
-            max_len = max(len(str(self.__whole(i))) for i in self)
+        if all((int(i) == i) for i in self):
+            max_len = max(len(str(int(i))) for i in self)
             if max_len <= 3:
-                return self.to_string(self.__show_as_whole, space=6)
+                return self.to_string(self.__show, width=6, precision=0)
             elif max_len <= 8:
-                return self.to_string(self.__show_as_whole, space=12)
-        return self.to_string(self.__show_as_float)
+                return self.to_string(self.__show, width=12, precision=0)
+        return self.to_string(self.__show)
         # todo: scientific notation for too big or too small
 
     @staticmethod
-    def __is_whole(item):
-        return int(item) == item
-
-    @staticmethod
-    def __whole(item):
-        return int(item)
-
-    @staticmethod
-    def __show_as_whole(item, space=6):
-        string = str(int(item))
-        vacancy = space - len(string)
-        return " " * vacancy + string
-
-    @staticmethod
-    def __show_as_float(item):
-        string = f"{item:.4f}"
-        vacancy = 10 - len(string)
-        return " " * vacancy + string
+    def __show(item, width=10, precision=4):
+        return f"{item:>{width}.{precision}f}"
