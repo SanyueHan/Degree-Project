191ebc3c9a8c7ac0bda5a37421c74e3ff419027b
Laniskefu <hansanyue@hotmail.com>
2021-03-29 20:21:03 +0800

    amend output in special cases

--- main/datatype/data/array_data/numeric_data/float.py
+++ main/datatype/data/array_data/numeric_data/float.py
@@ -6,6 +6,10 @@ class Float(Numeric):
     Class = DataClass.FLOAT
 
     def __str__(self):
+        if self.Size == (0, 0):
+            return "     []"
+        if self.n == 0:
+            return "  1×0 empty double row vector"
         if all((int(i) == i) for i in self):
             max_len = max(len(str(int(i))) for i in self)
             if max_len <= 3:
