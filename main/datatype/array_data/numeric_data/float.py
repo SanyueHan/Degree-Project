d8483e38aebb966a68d345be3173c6d275f8541c
Laniskefu <hansanyue@hotmail.com>
2021-03-31 22:45:57 +0800

    amend datatype

--- main/datatype/array_data/numeric_data/float.py
+++ main/datatype/array_data/numeric_data/float.py
@@ -2,26 +2,4 @@ from main.datatype.array_data.numeric import Numeric
 
 
 class Float(Numeric):
-    def __str__(self):
-        if self.Size == (0, 0):
-            return "     []"
-        if self.n == 0:
-            return "  1×0 empty double row vector"
-        if all((int(i) == i) for i in self):
-            max_len = max(len(str(int(i))) for i in self)
-            if max_len <= 3:
-                return self.to_string(self.__show, width=6, precision=0)
-            elif max_len <= 8:
-                return self.to_string(self.__show, width=12, precision=0)
-        return self.to_string(self.__show)
-        # todo: scientific notation for too big or too small
-
-    @staticmethod
-    def __show(item, width=10, precision=4):
-        return f"{item:>{width}.{precision}f}"
-
-
-if __name__ == "__main__":
-    print(Float([]).get_class())
-    print(Float([]).get_super())
-    print(Float([]).get_name())
+    pass
--- main/datatype/array_data/numeric_data/float_data/double.py
+++ main/datatype/array_data/numeric_data/float_data/double.py
@@ -5,3 +5,21 @@ class Double(Float):
     def __init__(self, value, size=None):
         super().__init__(value, size)
         self.Value = [float(i) for i in self]
+
+    def __str__(self):
+        if self.Size == (0, 0):
+            return "     []"
+        if self.n == 0:
+            return "  1×0 empty double row vector"
+        if all((int(i) == i) for i in self):
+            max_len = max(len(str(int(i))) for i in self)
+            if max_len <= 3:
+                return self.to_string(self.__show, width=6, precision=0)
+            elif max_len <= 8:
+                return self.to_string(self.__show, width=12, precision=0)
+        return self.to_string(self.__show)
+        # todo: scientific notation for too big or too small
+
+    @staticmethod
+    def __show(item, width=10, precision=4):
+        return f"{item:>{width}.{precision}f}"
