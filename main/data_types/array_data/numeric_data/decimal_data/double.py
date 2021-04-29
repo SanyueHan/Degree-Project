88e985c626648ea819ddaa77f21ff9c5a2c2206e
tt672 <1419126206@qq.com>
2021-04-29 17:45:56 +0800

    scientific notation for double

--- main/data_types/array_data/numeric_data/decimal_data/double.py
+++ main/data_types/array_data/numeric_data/decimal_data/double.py
@@ -11,7 +11,7 @@ class Double(Decimal):
     def __str__(self):
         if self.size == (0, 0):
             return "     []"
-        if self.n == 0:
+        if self.n == 0:  # 1：0.1
             return "  1x0 empty double row vector"
         if all((int(n) == n) for n in self):
             max_len = max(self.excluding_sign_integer_length(n) for n in self)
@@ -19,19 +19,64 @@ class Double(Decimal):
                 return self.pile(self.format_setter(width=6, precision=0))
             elif max_len <= 8:
                 return self.pile(self.format_setter(width=12, precision=0))
-        return self.pile(self.format_setter())
-        # todo: scientific notation for too big or too small
+            else:
+                return self.pile(self.format_setter(width=13, sci=True))
+        if not self.need_scientific():
+            return self.pile(self.format_setter())
+        if self.n == 1:
+            return self.pile(self.format_setter(width=13, sci=True))
+        return self.sci_representation()
+
+    # todo: Finished!!!scientific notation for too big or too small
+    def need_scientific(self):
+        count = 0
+        for n in self:
+            if int(n) == n and self.excluding_sign_integer_length(n) < 9:
+                continue
+            if int(n) == n and self.excluding_sign_integer_length(n) >= 9:
+                return True
+            if abs(n) > 999.9999:
+                return True
+            if abs(n) <= 0.001:
+                count += 1
+        return count == self.__len__()
+
+    def sci_representation(self):
+        _min = _max = self[0]
+        for n in self:
+            _min = min(abs(n), _min)
+            _max = max(abs(n), _max)
+        if _max >= 1000:
+            base = self.get_sci_base(_max, True)
+        else:
+            base = self.get_sci_base(_max, False)
+        result = f'{(10**base):>{10}.{1}e}' + " *\n\n"
+        fun = self.format_setter()
+        result += ''.join([''.join([fun(v/(10**base)) for v in r]) for r in self.rows()])
+        return result
+
+    @staticmethod
+    def get_sci_base(num, positive):
+        base = 2 if positive else -2
+        delta = 1 if positive else -1
+        while (10**(base + delta) < num and positive) or (10**(base + delta) > num and not positive):
+            base += delta
+        return base
 
     @staticmethod
     def excluding_sign_integer_length(number):
-        string = str(int(number)).lstrip('-')
+        string = str(int(number)).lstrip('-')  # 删掉-
         return len(string)
 
     @staticmethod
-    def format_setter(width=10, precision=4):
-        if precision == 0:
+    def format_setter(width=10, precision=4, sci=False):
+        if sci:
             def fun(item):
-                return f"{int(item):>{width}d}"
+                return f"{item:>{width}.{precision}e}"
+            return fun
+        elif precision == 0:
+            def fun(item):
+                return f"{int(item):>{width}d}"  # format
             return fun
         else:
             def fun(item):
