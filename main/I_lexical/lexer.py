467df1942ce6f32f0fe2616536b8c4760d48b8d6
tt672 <1419126206@qq.com>
2021-05-04 17:25:31 +0800

    Whitespace and Minus operation in array

--- main/I_lexical/lexer.py
+++ main/I_lexical/lexer.py
@@ -10,6 +10,8 @@ def lexer(program):
     for line in lines:
         col = 1
         text = ''
+        brackets_depth = 0
+        last = None
         while line:
             for TYPE in TokenType:
                 match = TYPE.value.match(line)
@@ -20,14 +22,23 @@ def lexer(program):
                     if TYPE == TokenType.TRA:
                         if TokenType.WHITESPACE.value.sub('', text) == '':
                             raise CharacterVectorTerminationError(row, col)
-
                     text = match.group()
                     line = line[len(text):]
-                    if TYPE != TokenType.WHITESPACE and TYPE != TokenType.ANNOTATION:
+                    if TYPE == TokenType.ADD and brackets_depth > 0 and last == TokenType.WHITESPACE:
+                        result_token_list.append(Token(t_type=TokenType.EO_STMT, t_text=',', row=row, col=col - 1))
+                        result_token_list.append(Token(t_type=TokenType.ADD, t_text=text, row=row, col=col))
+                    elif TYPE == TokenType.WHITESPACE and last == TokenType.ADD and brackets_depth > 0 and result_token_list.__getitem__(result_token_list.__len__() - 2).get_type() == TokenType.EO_STMT:  # space - space就要删掉减号前的逗号
+                        result_token_list.remove(result_token_list.__getitem__(result_token_list.__len__() - 2))
+                    elif TYPE != TokenType.WHITESPACE and TYPE != TokenType.ANNOTATION:
                         result_token_list.append(Token(t_type=TYPE, t_text=text, row=row, col=col))
+                    if TYPE == TokenType.L_BRACKET:
+                        brackets_depth += 1
+                    if TYPE == TokenType.R_BRACKET:
+                        brackets_depth -= 1
+                    last = TYPE
                     col += len(text)
                     break
-            else:
+            else:   # 抛出异常
                 if line[0] in "#$`":
                     raise InvalidCharacterError(row, col)
                 elif line[0] == '\"':
@@ -35,5 +46,4 @@ def lexer(program):
                 else:
                     raise InvalidCharacterError(row, col)
         row += 1
-
     return result_token_list
