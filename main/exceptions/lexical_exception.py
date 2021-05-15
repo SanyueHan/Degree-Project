6086cc6932599d278981c8c17cb45089d38ffed8
Laniskefu <hansanyue@hotmail.com>
2021-05-15 19:54:19 +0800

    fit error message difference in different platform

--- main/exceptions/lexical_exception.py
+++ main/exceptions/lexical_exception.py
@@ -5,7 +5,8 @@ class InvalidCharacterError(InterpretException):
     message = {
         'darwin': "Invalid text character. Check for unsupported symbol, invisible character, or\n"
                   "pasting of non-ASCII characters.",
-        'win32': "Invalid text character. Check for unsupported symbol, invisible character, or pasting of non-ASCII characters."
+        'win32': "Invalid text character. Check for unsupported symbol, invisible character, or pasting of non-ASCII "
+                 "characters. "
     }
 
 
@@ -13,7 +14,6 @@ class StringTerminationError(InterpretException):
     message = {
         'darwin': "String is not terminated properly.",
         'win32': "String is not terminated properly."
-
     }
 
 
