9b95ecde07ce507d90bf54a0f961fabba1afe036
ezy0812 <ezy0812@163.com>
2021-05-15 19:41:14 +0800

    add win32 lexi error

--- main/exceptions/lexical_exception.py
+++ main/exceptions/lexical_exception.py
@@ -4,17 +4,21 @@ from main.exceptions.interpret_exception import InterpretException
 class InvalidCharacterError(InterpretException):
     message = {
         'darwin': "Invalid text character. Check for unsupported symbol, invisible character, or\n"
-                  "pasting of non-ASCII characters."
+                  "pasting of non-ASCII characters.",
+        'win32': "Invalid text character. Check for unsupported symbol, invisible character, or pasting of non-ASCII characters."
     }
 
 
 class StringTerminationError(InterpretException):
     message = {
-        'darwin': "String is not terminated properly."
+        'darwin': "String is not terminated properly.",
+        'win32': "String is not terminated properly."
+
     }
 
 
 class CharacterVectorTerminationError(InterpretException):
     message = {
-        'darwin': "Character vector is not terminated properly."
+        'darwin': "Character vector is not terminated properly.",
+        'win32': "Character vector is not terminated properly."
     }
